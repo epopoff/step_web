2154e1554b9c546da07621f9b307d98ad9a37809
epopoff <pop.off.evgeny@gmail.com>
2017-01-20 14:13:28 +0300

    no message

--- hello.py
+++ hello.py
@@ -1,12 +1,9 @@
-def application(environ, start_response):
-	# бизнес-логика
+def application (environ, start_response):
 	resp = environ['QUERY_STRING'].split('&')
 	resp = [item + '\r\n' for item in resp]
-	# 
 	status = '200 OK'
 	headers = [
 		('Content-Type', 'text/plain')
 	]
 	start_response(status, headers)
 	return resp
-	
